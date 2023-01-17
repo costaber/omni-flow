a5cfb469b64370ed2b5280c88b99cc860bfdcdd8
Bernardo Costa <bernardo.costa@mercedes-benz.io>
2023-01-17 16:03:16 +0000

    feat: creation the model of a complete scope

---
+++ src/main/kotlin/org/example/challenge/faas/builder/ScopeBuilder.kt
@@ -0,0 +1,14 @@
+package org.example.challenge.faas.builder
+
+class ScopeBuilder : Builder<Unit> {
+
+    private val workflows: MutableList<WorkflowBuilder> = mutableListOf()
+
+    fun deployable(vararg workflow: WorkflowBuilder) {
+        workflows.addAll(workflow)
+    }
+    override fun build() {
+        workflows.map { it.build() }
+            .forEach { println(it) }
+    }
+}
--- src/main/kotlin/org/example/challenge/faas/builder/WorkflowBuilder.kt
+++ src/main/kotlin/org/example/challenge/faas/builder/WorkflowBuilder.kt
@@ -1,22 +1,35 @@
 package org.example.challenge.faas.builder
 
 import org.example.challenge.faas.domain.Workflow
+import org.example.challenge.faas.domain.getCloudProvider
 
 class WorkflowBuilder : Builder<Workflow> {
 
+    private lateinit var zone: String
+    private lateinit var provider: String
     private lateinit var name: String
     private lateinit var description: String
+    private lateinit var result: String
     private val steps = mutableListOf<StepBuilder>()
+
+    fun zone(value: String) = apply { this.zone = value }
+
+    fun provider(value: String) = apply { this.provider = value }
 
     fun name(value: String) = apply { this.name = value }
 
     fun description(value: String) = apply { this.description = value }
 
-    fun step(stepBuilder: StepBuilder) = apply { this.steps.add(stepBuilder) }
+    fun step(value: StepBuilder) = apply { this.steps.add(value) }
+
+    fun result(value: String) = apply { this.result = value }
 
     override fun build() = Workflow(
+        zone = zone,
+        provider = getCloudProvider(provider),
         name = name,
         description = description,
-        definition = steps.map { it.build() }
+        definition = steps.map { it.build() },
+        result = result
     )
 }
