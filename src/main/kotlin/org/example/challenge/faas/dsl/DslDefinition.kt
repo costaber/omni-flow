d37b83ede73ef05d4384a4d00c6284f4a50116db
Bernardo Costa <bjmcosta97@gmail.com>
2023-01-18 16:23:51 +0000

    feat: create architecture for dsl (#2)
    * feat: add domain builders
    
    * feat: add DSL defenition
    
    * feat: add DSL definition
    
    * feat: creation the model of a complete scope
    
    * refactor: clean code
    
    * refactor: improve code
    
    * docs: clean ToDo list
    
    Co-authored-by: Bernardo Costa <bernardo.costa@mercedes-benz.io>

---
+++ src/main/kotlin/org/example/challenge/faas/dsl/DslDefinition.kt
@@ -0,0 +1,17 @@
+package org.example.challenge.faas.dsl
+
+import org.example.challenge.faas.builder.StepBuilder
+import org.example.challenge.faas.builder.WorkflowBuilder
+import org.example.challenge.faas.builder.execution.ExecutionContextBuilder
+
+fun workflow(workflowBuilder: WorkflowBuilder.() -> Unit): WorkflowBuilder {
+    return WorkflowBuilder().apply(workflowBuilder)
+}
+
+fun step(stepBuilder: StepBuilder.() -> Unit): StepBuilder {
+    return StepBuilder().apply(stepBuilder)
+}
+
+fun execution(executionContextBuilder: ExecutionContextBuilder.() -> Unit): ExecutionContextBuilder {
+    return ExecutionContextBuilder().apply(executionContextBuilder)
+}
