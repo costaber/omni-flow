8bbfe7011a420fd8aa1b3aac5483a2bc6929c50f
Bernardo Costa <bernardo.costa@mercedes-benz.io>
2023-02-28 11:24:18 +0000

    feat: do small improvements

--- src/main/kotlin/org/example/challenge/faas/domain/execution/ExecutionContext.kt
+++ src/main/kotlin/org/example/challenge/faas/domain/execution/ExecutionContext.kt
@@ -3,12 +3,12 @@ package org.example.challenge.faas.domain.execution
 import org.example.challenge.faas.domain.Context
 
 data class ExecutionContext(
-    val url: String,
     val method: String,
+    val result: String,
+    val url: String,
+    val authentication: Authentication? = null,
+    val body: Map<String, String> = emptyMap(),
     val header: Map<String, String> = emptyMap(),
     val query: Map<String, String> = emptyMap(),
-    val body: Map<String, String> = emptyMap(),
-    val authentication: Authentication? = null,
     val timeoutInSeconds: Long? = null,
-    val result: String,
 ) : Context
