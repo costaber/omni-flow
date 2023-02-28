2bdbcd3775016de8489c4662eab8fdfe251b21ac
Bernardo Costa <bernardo.costa@mercedes-benz.io>
2023-02-28 11:24:18 +0000

    feat: do small improvements

--- src/main/kotlin/org/example/challenge/faas/domain/Step.kt
+++ src/main/kotlin/org/example/challenge/faas/domain/Step.kt
@@ -1,15 +1,8 @@
 package org.example.challenge.faas.domain
 
-open class Step(
+data class Step(
     val name: String,
     val description: String,
     val type: StepType = StepType.EXECUTION,
     val context: Context,
-) {
-
-    override fun toString() = "${Step::class.simpleName}(" +
-            "${Step::name.name}=$name, " +
-            "${Step::description.name}=$description, " +
-            "${Step::type.name}=$type, " +
-            "${Step::context.name}=$context )"
-}
+)
