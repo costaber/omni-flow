ad53adbd20901624d38072f53430dd1c74c3151b
Bernardo Costa <bjmcosta97@gmail.com>
2023-06-14 01:49:14 +0100

    feat: create workflow generator

---
+++ benchmark/src/main/kotlin/costaber/com/github/omniflow/generator/WorkflowGenerator.kt
@@ -0,0 +1,11 @@
+package costaber.com.github.omniflow.generator
+
+import costaber.com.github.omniflow.model.Workflow
+
+class WorkflowGenerator {
+
+    fun generate(number: Int): Collection<Workflow> {
+        return emptyList()
+    }
+
+}
